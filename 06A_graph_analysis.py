8a5730c53b92bd9669f9c0cb58d1f2f6b70c3718
wyu <kharma@gmail.com>
2018-12-02 09:57:36 -0800

    update module for presentation

--- 06A_graph_analysis.py
+++ 06A_graph_analysis.py
@@ -1,17 +1,49 @@
-# Module 6A - Spark and Neo4J Setup:
+# Module 6A - Spark and GraphX Setup:
 # Reference: Lecture 11 lecture notes and Lab 11 notebook
 
+# Module 6A - Spark and GraphX Setup:
+# Reference: Lecture 11 lecture notes and Lab 11 notebook
+from pyspark.sql import SparkSession
+from pyspark import SparkContext, SparkConf
+from pyspark import SparkContext as sc
+from pyspark.sql import SQLContext
+from pyspark.sql.functions import *
+
+appName = "nhts_graph"
+spark = SparkSession.builder.appName(appName).config('spark.jars.packages','graphframes:graphframes:0.6.0-spark2.3-s_2.11').getOrCreate()
+
+# Load CSV data:
+hhpub_gx = spark.read.option("header","true")\
+    .csv("./data/hhpub.csv")
+perpub_gx = spark.read.option("header","true")\
+    .csv("./data/perpub.csv")
+trippub_gx = spark.read.option("header","true")\
+    .csv("./data/trippub.csv")
+vehpub_gx = spark.read.option("header","true")\
+    .csv("./data/vehpub.csv")
+
+print('Total Household Count:')
+print(hhpub_gx.count())
+print('Total People Count:')
+print(perpub_gx.count())
+print('Total Trip Count:')
+print(trippub_gx.count())
+print('Total Vehicle Count:')
+print(vehpub_gx.count())
+
 # Set vertices/edges:
-hh_vertices = hhpub
-trip_edges = trippub
-hh_vertices = hhpub.withColumnRenamed("CDIVMSAR", "id").distinct()
-trip_edges = trippub\
+hh_vertices = hhpub_gx
+trip_edges = trippub_gx
+hh_vertices = hhpub_gx.withColumnRenamed("CDIVMSAR", "id").distinct()
+trip_edges = trippub_gx\
     .withColumnRenamed("CDIVMSAR", "src")\
     .withColumnRenamed("VMT_MILE", "dst")
 
 # Create graph:
+# spark = SparkSession.builder.appName(appName).config('spark.jars.packages','graphframes:graphframes:0.6.0-spark2.3-s_2.11').getOrCreate()
+# !pyspark --packages graphframes:graphframes:0.6.0-spark2.3-s_2.11
 from graphframes import *
-graph = GraphFrame(hh_vertices, trip_edges)
+graph = GraphFrame(hh_vertices, trip_edges  )
 type(graph)
 graph.cache()
 
@@ -20,7 +52,6 @@ print("Total Number of Households: " + str(graph.vertices.count()))
 print("Total Number of Relationships in Graph: " + str(graph.edges.count()))
 print("Total Number of Relationships in Original Data: " + str(trip_edges.count()))
 
-from pyspark.sql.functions import desc
 print('Show edges:')
 graph.edges.groupBy("src", "dst").count().orderBy(desc("count")).show(50)
 
